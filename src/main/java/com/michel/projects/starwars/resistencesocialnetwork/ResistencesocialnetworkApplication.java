5bfa0faa7a642e6bee5fecb4d639f7cc30ee03d8
Michel Ferreira <michel.ferreira.silva@gmail.com>
2021-10-22 09:06:56 -0300

    initial commit

---
+++ src/main/java/com/michel/projects/starwars/resistencesocialnetwork/ResistencesocialnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.michel.projects.starwars.resistencesocialnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResistencesocialnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResistencesocialnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/michel/projects/starwars/resistencesocialnetwork/ResistencesocialnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michel.projects.starwars.resistencesocialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResistencesocialnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
